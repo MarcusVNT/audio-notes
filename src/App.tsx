b36394b30666e9523fb9a71ea9594a3c537a52f6
Marcus Vinícius <marcus-vnt@live.com>
2024-02-07 17:19:42 -0300

    Favicon and Logo NLW added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,30 @@
+import { NewNoteCard } from "./Components/NewNoteCard";
+import { NoteCard } from "./Components/NoteCards";
+import LogoNLW from "./assets/LogoNLW.svg";
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={LogoNLW} alt="Logo NLW" />
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busquem em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500 "
+        />
+      </form>
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
+  );
 }
