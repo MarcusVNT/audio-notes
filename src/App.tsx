3ef4d6659c0ae2339a953118735c91c2a4fbe8ea
Marcus Vinícius <marcus-vnt@live.com>
2024-02-09 00:54:10 -0300

    Search input added.

--- src/App.tsx
+++ src/App.tsx
@@ -36,12 +36,25 @@ export function App() {
     localStorage.setItem("notes", JSON.stringify(notesArray));
   }
 
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+    setSearch(query);
+  }
+
+  const filteredNotes =
+    search !== ""
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )
+      : notes;
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={LogoNLW} alt="Logo NLW" />
       <form className="w-full">
         <input
           type="text"
+          onChange={handleSearch}
           placeholder="Busquem em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500 "
         />
@@ -50,7 +63,7 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
-        {notes.map((note) => {
+        {filteredNotes.map((note) => {
           return <NoteCard key={note.id} note={note} />;
         })}
       </div>
